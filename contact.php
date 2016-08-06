5ec6a14ba59b9d651d371a61b26e6a0470816a6f
Ajay verma <aries.ajay1@outlook.com>
2016-08-07 02:35:51 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,18 +1,11 @@
 <?php
- 
+
 if(isset($_POST['email'])) {
- 
-     
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
+
     $email_to = "aries.ajay1@outlook.com";
  
-    $email_subject = "IMP-New message from your ODiPi site.";
- 
-     
- 
-     
+    $email_subject = "ODP Website Contact";
+
  
     function died($error) {
  
@@ -37,7 +30,7 @@ if(isset($_POST['email'])) {
     if(!isset($_POST['name']) ||
  
         !isset($_POST['email']) ||
-        
+ 
         !isset($_POST['message'])) {
  
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
@@ -45,45 +38,10 @@ if(isset($_POST['email'])) {
     }
  
      
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
  
-    $name = $_POST['name']; // required
- 
-    $email_from = $_POST['email']; // required
-    
-    $message = $_POST['message']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
- 
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
- 
-  }
- 
- 
-  if(strlen($message) < 2) {
- 
-    $error_message .= 'The message you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
  
     $email_message = "Form details below.\n\n";
  
@@ -101,24 +59,23 @@ if(isset($_POST['email'])) {
  
     $email_message .= "Name: ".clean_string($name)."\n";
  
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    
+    $email_message .= "Email: ".clean_string($email)."\n";
+ 
     $email_message .= "Message: ".clean_string($message)."\n";
+
  
-     
- 
-     
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
+	// create email headers
+	 
+	$headers = 'From: '.$email."\r\n".
+	 
+	'Reply-To: '.$email."\r\n" .
+	 
+	'X-Mailer: PHP/' . phpversion();
+	 
+	@mail($email_to, $email_subject, $email_message, $headers);  
+
+
+?>
  
  
  
@@ -126,8 +83,12 @@ $headers = 'From: '.$email_from."\r\n".
  
  
  
-Thank you for contacting. Hoping to get back to you soon.
+Thank you for contacting me. Will be in touch with you very soon.
  
  
- ?>
-
+ 
+<?php
+ 
+}
+ 
+?>
