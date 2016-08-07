a289407eed4e53f4e080ff8d8a67c5869413e8fc
Ajay verma <aries.ajay1@outlook.com>
2016-08-07 07:39:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -38,12 +38,12 @@
                 <ul class="right hide-on-med-and-down">
                     <li><a href="register.php">Sign up/Sign in</a></li>
                     <li><a href="#intro">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact" onClick="document.getElementById('icon_prefix').focus();">Contact</a></li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
                     <li><a href="register.php">Sign up/Sign in</a></li>
                     <li><a href="#intro">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact" onClick="document.getElementById('icon_prefix').focus();">Contact</a></li>
                 </ul>
             <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons mdi-navigation-menu"></i></a>
             </div>
